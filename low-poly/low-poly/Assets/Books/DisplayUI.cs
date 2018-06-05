41cdc1d09cc4b8ca81d30a7171526f6e97c02101
Michael Mendoza <m.mendoza711@gmail.com>
2018-06-05 17:11:00 -0600

    Updated scenes. Added coliders scene.

---
+++ low-poly/low-poly/Assets/Books/DisplayUI.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DisplayUI : MonoBehaviour {
+
+    public GameObject Tooltip;
+
+    public void Update()
+    {
+        
+    }
+
+    private void OnMouseEnter()
+    {
+        Tooltip.SetActive(true);
+        //Tooltip.transform.LookAt(Camera.main.transform.position);
+    }
+
+    public void OnMouseExit()
+    {
+        Tooltip.SetActive(false);
+    }
+}
